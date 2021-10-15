dedb25139bbd67a86064304fe774fa7a67657ea1
SUDIPTA\91987 <psudipto2@gmail.com>
2021-10-16 00:04:07 +0530

    Flip and Run animation added through Script

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Animator animator;
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(speed);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(speed);
+        }
+        transform.localScale = scale;
     }
 }
