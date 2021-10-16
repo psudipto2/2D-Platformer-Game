1dbd87a976bdc39471e207e001b59a5d5fa4e0e0
SUDIPTA\91987 <psudipto2@gmail.com>
2021-10-16 23:21:56 +0530

    Player Movement Added

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,33 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public Animator animator;
+    [SerializeField]public float speed;
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        MoveCharacter(horizontal);
+        PlayerAnimation(horizontal);
+        //Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayerAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
-            scale.x = -3f * Mathf.Abs(speed);
+            scale.x = -3f * Mathf.Abs(horizontal);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
-            scale.x = 3*Mathf.Abs(speed);
+            scale.x = 3 * Mathf.Abs(horizontal);
         }
         transform.localScale = scale;
     }
